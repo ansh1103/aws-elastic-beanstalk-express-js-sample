cb922a93dd9a43148dc345bdea7c2f1b09084dcc
Anshuman Saikia <asaikia@synamedia.com>
2021-03-20 13:38:08 +0530

    new text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Happy learning AWS!!'));
+app.get('/', (req, res) => res.send('Congratulation!! Full Pipeline Test performed using aws'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
